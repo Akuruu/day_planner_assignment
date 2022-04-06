67e385525b5acf0fa1158c50414f297754732b8f
Akuru <anjalismith0529@gmail.com>
2022-04-06 18:39:59 -0400

    created base save button function

--- assets/script.js
+++ assets/script.js
@@ -3,11 +3,16 @@ var time = moment();
 $("#currentDay").text(time.format("MMMM Do YYYY, h:mm a"));
 
 // Begin a function using jQuery syntax
-$document.ready(function() {
+$(document).ready(function() {
     //listen for savebtn clicks
-
+    $(".saveBtn").on("click", function() {
     //show notification that the item was saved in localstg
     //more code for this item styling
+    var value = $(this).sibling(".information").val();
+    var time = $(this).parent().attribute("id");
+    localStorage.setItem(time, value)
+    })
+
 
     //function to update the current hour and change the colors of the time blocks
     function colorUpdater() {
